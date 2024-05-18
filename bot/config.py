b17e07b6d59b94ae6ff077abead96a7c741f310f
Ak129 <64674981+Ak129@users.noreply.github.com>
2024-05-18 14:46:29 +0530

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,25 +1,25 @@
 # Telegram API ID and Hash
-API_ID = int("telegram_api_id")
-API_HASH = "telegram_api_hash"
+API_ID = int("26562289")
+API_HASH = "d7426fd6f91cff563d62d318a7e5aab1"
 
 # Bot which will handle users commands
-MAIN_BOT_TOKEN = "main_bot_token"
+MAIN_BOT_TOKEN = "7078603039:AAGeD5aKgCaWv3lNQYvlX7kn7QF7LsgDWIY"
 
-LOGGER_BOT_TOKEN = "logger_bot_token"
+LOGGER_BOT_TOKEN = "6428237666:AAH7jKRiMpSYaj_wN9fk2iX0IbP_efaKpCE"
 
 # Bot tokens for STORAGE_CHANNEL_1, they all must be admin in the channel
-UPLOADER_BOTS_1 = ["bot_token1", "bot_token2", "bot_token3"]
+UPLOADER_BOTS_1 = ["7125718393:AAEUAuGs5bwOr-UUTCUZ-28hEB6MEsACHsg", "6492689749:AAG0AYVUzy3HWP3NMj-MjnLNCrOJyxk3lhg", "6904109249:AAESKb4N8h39u3a2drfA7VEgNSQrRIj71w4"]
 
 # Bot tokens for STORAGE_CHANNEL_2, they all must be admin in the channel
-UPLOADER_BOTS_2 = ["bot_token4", "bot_token5", "bot_token6"]
+UPLOADER_BOTS_2 = ["7125718393:AAEUAuGs5bwOr-UUTCUZ-28hEB6MEsACHsg", "6492689749:AAG0AYVUzy3HWP3NMj-MjnLNCrOJyxk3lhg", "6904109249:AAESKb4N8h39u3a2drfA7VEgNSQrRIj71w4"]
 
 # Telegram Channel ID where videos will be stored
-VIDEO_STORAGE = -1001234567890
+VIDEO_STORAGE = -1002104816396
 # Telegram Channel ID where logs will be stored
-LOGGER_CHANNEL = -1001234567890
+LOGGER_CHANNEL = -1002127693356
 # Telegram Channel ID where m3u8 file will be stored
-STORAGE_CHANNEL_1 = -1001234567890
-STORAGE_CHANNEL_2 = -1001234567890
+STORAGE_CHANNEL_1 = -1002081455838
+STORAGE_CHANNEL_2 = -1002005855977
 
 # Max no. of tasks to run simultaneously
 MAX_ACTIVE_TASKS = 5
@@ -40,9 +40,9 @@ OWNER_ID = 1234567890
 WEBSITE_DOMAIN = "https://stream.mywebsite.com"
 
 # MongoDB Database URL
-MONGO_DB_URL = "YOUR_MONGODB_URL"
+MONGO_DB_URL = "mongodb+srv://opmods4u:opmods4u@cluster0.oyil1qc.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 
 # Playerx.stream Login details
-PLAYERX_EMAIL = "your_email@gmail.com"
-PLAYERX_PASSWORD = "your_password"
-PLAYERX_API_KEY = "your_api"
+PLAYERX_EMAIL = "apkthugs@gmail.com"
+PLAYERX_PASSWORD = "Ak098765"
+PLAYERX_API_KEY = "rDsePmX39v1nCJA7"
